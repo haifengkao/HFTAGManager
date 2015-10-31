3c3e22245c3c676668cf856446e6eb0500c79447
Hai Feng Kao <haifeng@cocoaspice.in>
2015-10-31 15:01:20 +0800

    - add unit test file for HFTAGManager

---
+++ Example/Tests/HFTAGManagerSpec.m
@@ -0,0 +1,74 @@
+//
+//  HFTAGManagerSpec.m
+//  HFTAGManager
+//
+//  Created by Lono on 2015/10/29.
+//  Copyright 2015年 Hai Feng Kao. All rights reserved.
+//
+
+#import <Kiwi/Kiwi.h>
+#import "HFTAGManager.h"
+
+@interface ManagerTest : NSObject <HFTAGContainerCallback>
+@property NSDictionary* content;
+@property NSDictionary* userInfo;
+@property (assign) BOOL done;
+@end
+
+@implementation ManagerTest
+- (void)containerRefreshBegin:(HFTAGContainer *)container
+                  refreshType:(HFTAGContainerCallbackRefreshType)refreshType
+{
+}
+
+- (void)containerRefreshSuccess:(HFTAGContainer *)container
+                    refreshType:(HFTAGContainerCallbackRefreshType)refreshType
+{
+    [[container.container should] equal:self.content];
+    self.done = YES;
+}
+
+- (void)containerRefreshFailure:(HFTAGContainer *)container
+                        failure:(HFTAGContainerCallbackRefreshFailure)failure
+                    refreshType:(HFTAGContainerCallbackRefreshType)refreshType
+{
+}
+
+- (void)loadContainerWithId:(NSString*)containerId
+                   content:(void(^)(NSDictionary*))content
+                  userInfo:(void(^)(NSDictionary*))userInfo
+                     error:(void(^)(NSError*))error
+{
+    self.content = @{@"hello": @"world"};
+    content(self.content);
+    userInfo(nil);
+    error(nil);
+}
+@end
+
+SPEC_BEGIN(HFTAGManagerSpec)
+
+beforeAll(^{
+});
+afterAll(^{
+});
+afterEach(^{
+});
+
+describe(@"HFTAGManager", ^{
+    __block HFTAGManager* manager;
+    __block ManagerTest* callback;
+    beforeEach(^{ // Occurs before each enclosed "it"
+        manager = [HFTAGManager instance];
+        callback = [[ManagerTest alloc] init];
+        
+    });
+    
+    it(@"should open container", ^{
+        [manager openContainerById:@"unit test" callback:callback];
+        
+         [[expectFutureValue(@(callback.done)) shouldEventually] beYes];
+    });
+});
+
+SPEC_END
