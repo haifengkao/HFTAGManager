fab43e93004428b6bb78dc47f8c8a6969e57e0ca
Hai Feng Kao <haifeng@cocoaspice.in>
2015-11-05 14:00:19 +0800

    - add console test script

---
+++ test.sh
@@ -0,0 +1,1 @@
+xcodebuild -workspace Example/HFTAGManager.xcworkspace -scheme HFTAGManager-Example -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6,OS=9.1' test | xcpretty --test --color
