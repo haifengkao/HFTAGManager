e92730896ccd756ce0b144303811bf9f97e59765
Hai Feng Kao <haifeng@cocoaspice.in>
2015-11-26 19:25:33 +0800

    - update test cases

--- Example/Tests/HFTagContainerSpec.m
+++ Example/Tests/HFTagContainerSpec.m
@@ -33,6 +33,15 @@ describe(@"HFTAGContainer", ^{
             NSArray* arr = [container arrayForKey:badKey defaultRule:@[@"hi"]];
             [[arr should] equal:@[@"hi"]];
         });
+        it(@"should get default number", ^{
+            NSNumber* number = [container numberForKey:@"num_key" defaultRule:@(1)];
+            [[number should] equal:@(1)];
+        });
+        
+        it(@"should get default string", ^{
+            NSString* str = [container stringForKey:@"str_key" defaultRule:@"hi"];
+            [[str should] equal:@"hi"];
+        });
     });
     
     context(@"with rule", ^{
@@ -41,22 +50,131 @@ describe(@"HFTAGContainer", ^{
             rules = [NSMutableDictionary new];
             rules[@"dict_key"] = @[@[@"", @{@"hi2":@"hello"}]];
             rules[@"arr_key"] = @[@[@"", @[@"hi2"]]];
+            rules[@"num_key"] = @[@[@"", @(42)]];
+            rules[@"str_key"] = @[@[@"", @"hi2"]];
             container.container = rules;
         });
         
-        it(@"should get default dictionary", ^{
+        it(@"should not default dictionary", ^{
             NSDictionary* dict = [container dictionaryForKey:@"dict_key" defaultRule:@{@"hi":@"hello"}];
             [[dict should] equal:@{@"hi2":@"hello"}];
         });
         
-        it(@"should get default array", ^{
+        it(@"should not default array", ^{
             NSArray* arr = [container arrayForKey:@"arr_key" defaultRule:@[@"hi"]];
             [[arr should] equal:@[@"hi2"]];
+        });
+        
+        it(@"should not default number", ^{
+            NSNumber* number = [container numberForKey:@"num_key" defaultRule:@(1)];
+            [[number should] equal:@(42)];
+        });
+        
+        it(@"should not default string", ^{
+            NSString* str = [container stringForKey:@"str_key" defaultRule:@"hi"];
+            [[str should] equal:@"hi2"];
         });
     });
     
     context(@"with tag rule", ^{
         beforeEach(^{ // Occurs before each enclosed "it"
+        });
+        
+        context(@"with bad predicate", ^{
+            __block NSString* key = nil;
+            __block HFTAGRule* tagRule = nil;
+            __block NSDictionary* rule1;
+            __block NSDictionary* rule2;
+            beforeEach(^{ // Occurs before each enclosed "it"
+                key = @"test_key";
+                container.dataLayer = [HFTAGDataLayer new];
+                
+                rule1 = @{@(42): @"bar"};
+                rule2 = @{@"foo": @"bar"};
+                tagRule = [[HFTAGRule alloc] initWithBlock:^(HFTAGRule *rule) {
+                    [rule setPredicate:@"activatedTimes == 3" rule:rule1];
+                    [rule setPredicate:@"bad" rule:rule2];
+                }];
+            });
+            
+            it(@"should always get nil", ^{
+                NSDictionary* dict = [container dictionaryForKey:key defaultRule:tagRule];
+                [[dict should] beNil];
+            });
+            
+        });
+        
+         context(@"with bad datalayer", ^{
+            __block NSString* key = nil;
+            __block HFTAGRule* tagRule = nil;
+            __block NSDictionary* rule1;
+            __block NSDictionary* rule2;
+            beforeEach(^{ // Occurs before each enclosed "it"
+                key = @"test_key";
+                container.dataLayer = nil;
+                
+                rule1 = @{@(42): @"bar"};
+                rule2 = @{@"foo": @"bar"};
+                tagRule = [[HFTAGRule alloc] initWithBlock:^(HFTAGRule *rule) {
+                    [rule setPredicate:@"activatedTimes == 3" rule:rule1];
+                    [rule setPredicate:@"activatedTimes == 2" rule:rule2];
+                }];
+            });
+            it(@"should get nil", ^{
+                NSDictionary* dict = [container dictionaryForKey:key defaultRule:tagRule];
+                [[dict should] beNil];
+            });
+
+        });
+
+
+        context(@"when multiple context is matched", ^{
+            __block NSString* key = nil;
+            __block HFTAGDataLayer* dataLayer = nil;
+            __block HFTAGRule* tagRule = nil;
+            __block NSDictionary* rule1;
+            __block NSDictionary* rule2;
+            __block NSDictionary* rule3;
+            
+            beforeEach(^{ // Occurs before each enclosed "it"
+                key = @"test_key";
+                dataLayer = [HFTAGDataLayer new];
+                container.dataLayer = dataLayer;
+                
+                rule1 = @{@(42): @"bar"};
+                rule2 = @{@"foo": @"bar"};
+                rule3 = @{@"foz": @"baz"};
+                tagRule = [[HFTAGRule alloc] initWithBlock:^(HFTAGRule *rule) {
+                    [rule setPredicate:@"" rule:rule2];
+                    [rule setPredicate:@"activatedTimes == 3" rule:rule1];
+                    [rule setPredicate:@"YES == YES" rule:rule3];
+                }];
+            });
+            
+            it(@"should match the first rule", ^{
+                [dataLayer pushValue:@(3) forKey:@"activatedTimes"];
+                NSDictionary* dict = [container dictionaryForKey:key defaultRule:tagRule];
+                [[dict should] equal:rule1];
+            });
+            
+            it(@"should match the first matched rule", ^{
+                [dataLayer pushValue:@(5) forKey:@"no_this_value"];
+                NSDictionary* dict = [container dictionaryForKey:key defaultRule:tagRule];
+                [[dict should] equal:rule3];
+            });
+            
+            it(@"but the default rule always matched last", ^{
+                tagRule = [[HFTAGRule alloc] initWithBlock:^(HFTAGRule *rule) {
+                    [rule setPredicate:@"" rule:rule2];
+                    [rule setPredicate:@"activatedTimes == 3" rule:rule1];
+                    [rule setPredicate:@"no" rule:rule3];
+                }];
+                
+                [dataLayer pushValue:@(5) forKey:@"no_this_value"];
+                NSDictionary* dict = [container dictionaryForKey:key defaultRule:tagRule];
+                [[dict should] equal:rule2];
+            });
+            
         });
         
         context(@"with dictionary", ^{
